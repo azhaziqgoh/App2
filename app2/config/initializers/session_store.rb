0b3eca903edeb188023a9012013b15d5f2e99628
azhaziqgoh <md_azhaziq23@yahoo.com>
2014-02-27 13:30:11 +0800

    complete

--- app2/config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-App2::Application.config.session_store :cookie_store, key: '_app2_session'
--- app2/db/migrate/20140226085628_create_groups.rb
+++
@@ -1,9 +0,0 @@
-class CreateGroups < ActiveRecord::Migration
-  def change
-    create_table :groups do |t|
-      t.string :groupname
-
-      t.timestamps
-    end
-  end
-end
